3955e0fc2fd4abde0eb5d1201bdfa59f0be20fdd
TERRYYYY <kariuki.terry11@gmail.com>
2024-05-20 11:23:54 +0300

    Created comment entity and the table reflects on mySQL database

---
+++ src/main/java/com/terryProjects/blogServer/entity/Comment.java
@@ -0,0 +1,24 @@
+package com.terryProjects.blogServer.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Used for auto increment
+    private Long id;
+
+    private String content;
+
+    private Date createdAt;
+
+    private String postedBy;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+        private Post post;
+}
